5990c849114a4d5f7c1bd49735a668af0c237097
potatological <te.zahra@gmail.com>
2023-04-27 17:28:06 -0700

    update _printf.c

--- _printf.c
+++ _printf.c
@@ -6,52 +6,44 @@
  * Return: number of character printed
  */
 
-int _printf(const char *format, ... )
+int _printf(const char *format, ...)
 {
-  va_list arguments;
-  const char *frmt;
-  int i = 0;
-  char c_buff;
-  char *s_buff;
+  va_list ap;
+  char *ptr, *start;
+  int s = 0;
+  params_t params = PARAMS_INIT;
 
-  va_start(arguments, format);
+  va_start(ap, format);
 
-  for (frmt = format ; *frmt != '\0' ; frmt++)
+  if (!format || (format[0] == '%' && !format[1]))
+    return (-1);
+  if (format[0] == '%' && format[1] == ' ' && !format[2])
+    return (-1);
+  for (ptr = (char *)format; *ptr; ptr++)
     {
-      if (*frmt != '%')
+      init_params(&params, ap);
+      if (*ptr != '%')
 	{
-	  write(1, frmt, 1);
-	  i++;
+	  s += _putchar(*ptr);
+	  continue;
 	}
-      else if (*(frmt+1) == '%')
+      start = ptr;
+      ptr++;
+      while (get_flag(p, &params))
 	{
-	  write(1, "%", 1);
-	  i++;
-	  frmt++;
-	  if (*(frmt+1) == 'c')
-	    {
-	      c_buff = (char) va_arg(arguments, int);
-	      write(1, &c_buff, 1);
-	      i++;
-	      frmt++;
-	    }
-	  else if (*(frmt+1) == 's')
-	    {
-	      s_buff = va_arg(arguments, char*);
-	      for (; s_buff != '\0' ; s_buff++)
-		{
-		  write(1, s_buff, 1);
-		  i++;
-		}
-	    }
-	  else
-	    {
-	      write(2, "error: unknown format specifier\n", 32);
-	      va_end(arguments);
-	      return -1;
-	    }
+	  ptr++;
 	}
+      ptr = get_width(p, &params, ap);
+      ptr = get_precision(p, &params, ap);
+      if (get_modifier(p, &params))
+	ptr++;
+      if (!get_specifier(p))
+	s += print_from_to(start, p,
+			   params.l_modofier || params.h_modifier ? p - 1 : 0);
+      else
+	s += get_print_func(p, ap, &params);
     }
-  va_end(arguments);
-  return i;
+  _putchar(BUF_FLUSH);
+  va_end(ap);
+  return (s);
 }
